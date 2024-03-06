86cdd0b10889eeb1024665cccd4a22ef134ddd34
Makenzie Cope <mcope3208@gmail.com>
2024-03-06 09:12:31 -0600

    Pull table names from the database created

--- z3_retrieve.py
+++ z3_retrieve.py
@@ -2,5 +2,10 @@ import sqlite3
 conn = sqlite3.connect('databaseFile.db')
 cr = conn.cursor()
 
+cr.execute("SELECT name FROM sqlite_master WHERE  type='table';")
+tables = cr.fetchall()
+print(tables)
+
+#pulls data from table movies
 cr.execute("SELECT * FROM movies")
 print(cr.fetchall())
