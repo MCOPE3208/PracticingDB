4f6f6fb6a2e0b71e7fbd27f502066ba0035f83ab
Makenzie Cope <mcope3208@gmail.com>
2024-03-06 09:37:03 -0600

    update with the template to add new movies

--- z2_populatetable.py
+++ z2_populatetable.py
@@ -9,13 +9,6 @@ def add_movie(title,director,year,genre):
     conn.commit()
     print("Movie added :)")
 
+#template fr adding new movies:
 #add_movie("Inception", "Christopher Nolan", 2010, "Sci-Fi")
-add_movie("The Shawshank Redemption", "Frank Darabont", 1994, "Drama")
-add_movie("The Dark Knight", "Christopher Nolan", 2008, "Action")
-add_movie("Pulp Fiction", "Quentin Tarantino", 1994, "Crime")
-add_movie("Forrest Gump", "Robert Zemeckis", 1994, "Drama")
-add_movie("The Matrix", "Lana Wachowski, Lilly Wachowski", 1999, "Action")
-add_movie("Titanic", "James Cameron", 1997, "Romance")
-add_movie("The Godfather", "Francis Ford Coppola", 1972, "Crime")
-add_movie("Avatar", "James Cameron", 2009, "Action")
-add_movie("Interstellar", "Christopher Nolan", 2014, "Sci-Fi")
+#do not run the above inception has already been added
